0dd54330d85fe4fa6ef5e4157091b87bde484095
Raharo1234 <112761723+Raharo1234@users.noreply.github.com>
2022-09-03 22:35:04 +0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,150 @@
+let info = {
+  name: "Raharo ANDRIATSITOHAINA",
+  logo_name: "Raharo",
+  flat_picture: require("./src/assets/potrait.jpg"),
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false
+    }
+  },
+  description:
+    "I'm a Full Stack Developper with a passion for building projects that are used routinely. Building applications in JavaScript. Former supervisor and finance professional who's eager to enhance a company's user experience. ",
+
+  links: {
+    linkedin: "https://www.linkedin.com/in/raharo/",
+    github: "https://github.com/Raharo1234",
+  },
+  education: [
+    {
+      name: "Lycée Privée Ambinintsoa",
+      place: " Mahitsy Ambohitrimanjaka",
+      date: "2017 ",
+      degree: "Bacalaureat",
+
+      skills: [
+
+
+      ]
+    },
+    {
+      name: "Ecole National d'Informatique ",
+      course:"Full Stack Developer",
+      place: "Fianarantsoa",
+      date: "2021 ",
+      degree: "Licence",
+      gpa: "4.0/5.0",
+      description:
+        "",
+      skills: [
+        "HTML5",
+        "CSS3",
+        "JavaFX",
+        "PHP",
+        "MySql",
+       "Reactjs",
+       "MongoDB",
+       "postgreSQL"
+      ]
+    }
+  ],
+  experience: [
+      {
+      name: "InternShip",
+      place: "MICA-Fianarantsoa",
+      date: "2018-2019",
+      position: "Web Application",
+      description:
+       'Conception and developement of a software to human resource management',
+        skills: [
+          "HTML5",
+          "CSS3",
+          "Bootstrap",
+          "Symphony",
+        ]
+    },
+    {
+      name: "Personnal Project",
+      place: "Ecole National d'Informatique",
+      date: "2019-2020",
+      position: "Web application",
+      description:
+       'medical service management',
+        skills: [
+          "HTML5",
+          "CSS3",
+          "Bootstrap",
+          "Nodejs",
+        ]
+    },
+    {
+      name: "InternShip",
+      place: "Nix Bat Construction Antananarivo",
+      date: "march-June 2020",
+      position: "Desktop-App",
+      description:
+       ' Conception and developement of a software : "Calcul de feraillage"',
+        skills: [
+          "javaFX",
+          "scene builder"
+        ]
+    },
+
+
+  
+
+  ],
+  skills: [
+    {
+      title: "languages",
+      info: [
+        "Javascript",
+        "CSS",
+        "HTML5"
+
+      ],
+      icon: "fa fa-code"
+    },
+    {
+      title: "Web technologies",
+      info: ["Vue.js",  "React.js", "HTML", "CSS","Framer Motion","Boostrap","Material ui"],
+      icon: "fas fa-laptop-code"
+    },
+    {
+      title: "databases",
+      info: ["MongoDB", "MySQL","PostGreSql"],
+      icon: "fa fa-database"
+    },
+    {
+      title: "operating systems & tools",
+      info: [
+        "Windows",
+        "Android",
+        "Linux"
+      ],
+      icon: "fas fa-tools"
+    },
+    {
+      title: "design",
+      info: ["Illustrator", "XD", "Photoshop", "Figma"],
+      icon: "fa fa-pencil-square-o"
+    }
+  ],
+  portfolio: [
+
+  ],
+   recommendations: [
+    {
+      title:
+        "In his internship, ANDRIATSITOHAINA Raharo has demonstrated excellent learning ability, and with a dedicated, task oriented approach, he was able to complete his assignment ahead of time.",
+      author: "Mamy Nirina Andriamanana",
+      position: "Desktop-App",
+      company: "Nix Bat Contruction",
+    }
+  ]
+};
+
+export default info;
+
+
+
